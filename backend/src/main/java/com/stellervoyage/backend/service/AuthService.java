c8dbc1420b2204d36244473fba0dc0b159061257
madushan <ranasinhemadushan@gmail.com>
2023-08-15 04:30:24 +0530

    login and registration in auth service implemented

--- backend/src/main/java/com/stellervoyage/backend/service/AuthService.java
+++ backend/src/main/java/com/stellervoyage/backend/service/AuthService.java
@@ -1,12 +1,26 @@
 package com.stellervoyage.backend.service;
 
+import com.stellervoyage.backend.config.JwtService;
 import com.stellervoyage.backend.dto.LoginRequest;
 import com.stellervoyage.backend.dto.LoginResponse;
 import com.stellervoyage.backend.dto.RegistrationRequest;
+import com.stellervoyage.backend.model.Role;
+import com.stellervoyage.backend.model.User;
+import com.stellervoyage.backend.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class AuthService {
+
+    private final UserRepository repository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtService jwtService;
+    private final AuthenticationManager authenticationManager;
 
     /**
      * This method registers users in the system
@@ -15,8 +29,17 @@ public class AuthService {
      * @return LoginResponse
      */
     public LoginResponse register(RegistrationRequest request) {
-        // todo implement the register process
-        return null;
+        var user = User.builder()
+                .name(request.getName())
+                .email(request.getEmail())
+                .password(passwordEncoder.encode(request.getPassword()))
+                .role(Role.USER)
+                .build();
+        repository.save(user);
+        var jwtToken = jwtService.generateToken(user);
+        return LoginResponse.builder()
+                .accessToken(jwtToken)
+                .build();
     }
 
     /**
@@ -26,7 +49,17 @@ public class AuthService {
      * @return LoginResponse
      */
     public LoginResponse login(LoginRequest request) {
-        // todo implement the login process
-        return null;
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        request.getEmail(),
+                        request.getPassword()
+                )
+        );
+        var user = repository.findByEmail(request.getEmail())
+                .orElseThrow();
+        var jwtToken = jwtService.generateToken(user);
+        return LoginResponse.builder()
+                .accessToken(jwtToken)
+                .build();
     }
 }
