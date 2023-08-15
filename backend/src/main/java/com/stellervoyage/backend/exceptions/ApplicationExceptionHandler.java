39600c1ef25867a95a29fa356a5f3d4ab7451928
madushan <ranasinhemadushan@gmail.com>
2023-08-15 05:55:46 +0530

    Rest level Error handling implemented

---
+++ backend/src/main/java/com/stellervoyage/backend/exceptions/ApplicationExceptionHandler.java
@@ -0,0 +1,27 @@
+package com.stellervoyage.backend.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+@RestControllerAdvice
+public class ApplicationExceptionHandler {
+
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public Map<String, String> handleInvalidArgument(MethodArgumentNotValidException exception) {
+        return exception.getBindingResult().getFieldErrors()
+                .stream()
+                .collect(Collectors.toMap(FieldError::getField, FieldError::getDefaultMessage));
+    }
+
+
+}
