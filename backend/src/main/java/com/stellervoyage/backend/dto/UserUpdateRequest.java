b98403c83605c7275932861b2a9f711c2e0ae9f5
madushan <ranasinhemadushan@gmail.com>
2023-08-15 14:22:14 +0530

    user details get and update method created

---
+++ backend/src/main/java/com/stellervoyage/backend/dto/UserUpdateRequest.java
@@ -0,0 +1,22 @@
+package com.stellervoyage.backend.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserUpdateRequest {
+    @NotBlank(message = "name is required")
+    private String name;
+    @NotBlank(message = "Email is required")
+    @Email(message = "Invalid email format")
+    private String email;
+    private String natId;
+
+}
--- backend/src/main/java/com/stellervoyage/backend/repository/UserRepository.java
+++ backend/src/main/java/com/stellervoyage/backend/repository/UserRepository.java
@@ -4,9 +4,10 @@ import com.stellervoyage.backend.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
+import java.util.UUID;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
         Optional<User> findByEmail(String email);
-
         boolean existsByEmail(String email);
+        Optional<User> findByUserId(UUID id);
 }
