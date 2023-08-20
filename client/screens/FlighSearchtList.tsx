f3ccfa16a13d697b1cfc0a95007cf7ff3106f21a
madushan <ranasinhemadushan@gmail.com>
2023-08-20 14:14:23 +0530

    checkout destination carasole

--- client/screens/FlighSearchtList.tsx
+++ client/screens/FlighSearchtList.tsx
@@ -1,33 +1,41 @@
 import React from 'react';
-import { View, Text, FlatList, StyleSheet } from 'react-native';
+import {View, Text, FlatList, StyleSheet, TouchableOpacity} from 'react-native';
 import CommonView from './CommonView';
-import { common } from '../config/style';
-import { PricingCard } from '../components/PricingCard/PricingCard';
+import {common} from '../config/style';
+import {PricingCard} from '../components/PricingCard/PricingCard';
 
-const FlightSearchList = ({ route, navigation }) => {
-  const { flightList } = route.params;
+const FlightSearchList = ({route, navigation}) => {
+    const {flightList} = route.params;
 
-  const renderItem = ({ item }) => (
-    <PricingCard flightId={item.flightId} from={item.from}
-     to={item.to} departureDate={item.departureDate} arrivalDate={item.arrivalDate} 
-     returnDate={item.returnDate} travelMode={item.travelMode} 
-     flightMode={item.flightMode} price={item.price} />
-  );
+    const flightHandler = (item: FlightInfo) => {
+        navigation.navigate('Checkout', {
+            flight: item
+        });
+    }
 
-  return (
-    <CommonView>
-      <View style={[common.middleArea, common.topArea]}>
-        <Text style={[common.mainTitle, { color: 'white', marginBottom: 12 }]}>Search</Text>
-      </View>
-      <View >
-        <FlatList
-          data={flightList}
-          renderItem={renderItem}
-          keyExtractor={(item, index) => index.toString()}
-        />
-      </View>
-    </CommonView>
-  );
+    const renderItem = (item: FlightInfo) => (
+        <TouchableOpacity onPress={flightHandler}>
+            <PricingCard flightId={item.flightId} from={item.from}
+                         to={item.to} departureDate={item.departureDate} arrivalDate={item.arrivalDate}
+                         returnDate={item.returnDate} travelMode={item.travelMode}
+                         flightMode={item.flightMode} price={item.price}/>
+        </TouchableOpacity>
+    );
+
+    return (
+        <CommonView>
+            <View style={[common.middleArea, common.topArea]}>
+                <Text style={[common.mainTitle, {color: 'white', marginBottom: 12}]}>Search</Text>
+            </View>
+            <View>
+                <FlatList
+                    data={flightList}
+                    renderItem={renderItem}
+                    keyExtractor={(item, index) => index.toString()}
+                />
+            </View>
+        </CommonView>
+    );
 };
 
 export default FlightSearchList;
